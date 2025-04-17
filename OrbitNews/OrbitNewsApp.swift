e47866ad9b20fd0c602e24a1abe8960e91303aef
chrisballack <chris04140607077@gmail.com>
2025-04-17 09:00:45 -0500

    project start

---
+++ OrbitNews/OrbitNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitNewsApp.swift
+//  OrbitNews
+//
+//  Created by Maria Fernanda Paz Rodriguez on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct OrbitNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OrbitNewsTests/OrbitNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitNewsTests.swift
+//  OrbitNewsTests
+//
+//  Created by Maria Fernanda Paz Rodriguez on 17/04/25.
+//
+
+import Testing
+@testable import OrbitNews
+
+struct OrbitNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
